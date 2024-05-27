f6f665dd5b0c7fd16f771355713c74742aaf999e
MATTHIEU CHAILLOU <mchail09@salle1>
2024-05-27 15:06:28 +0200

    Maj emplacement fichiers admin

---
+++ vues/login_admin_user.php
@@ -0,0 +1,10 @@
+<?php 
+//require('blocs/header.php');
+
+
+echo "<h1>Se connecter :<h1>";
+echo "<a href='vues_admin/auth_admin.php'><button type='button'>Admin</a>";
+echo "<a href='auth_user.php'><button type='button'>Utilisateur</a>";
+
+
+//require('blocs/footer.php');
