af9203527582eae7fc4258cfa391ed48abfc3d42
Charles <70532837+cAptive5976@users.noreply.github.com>
2024-05-27 15:19:34 +0200

    Finalisation de la base de données #2

--- sql/db_creation.sql
+++ sql/db_creation.sql
@@ -5,22 +5,16 @@ CREATE TABLE users (
     classe varchar(64) NOT NULL,
     isadmin boolean DEFAULT FALSE
 );
-
-CREATE TABLE admin (
-    id int NOT NULL PRIMARY KEY,
-    prenom varchar(32),
-    nom varchar(32),
-    FOREIGN KEY (id) REFERENCES users(id)
-);
-
 CREATE TABLE reponse (
     id int PRIMARY KEY auto_increment,
-    enonce_reponse varchar(128)
+    question_id int,
+    enonce_reponse varchar(128),
+    FOREIGN KEY (question_id) references question(id)
 );
 
 CREATE TABLE question (
     id int NOT NULL PRIMARY KEY auto_increment,
-    enonce varchar(128),
+    enonce varchar(255),
     reponse_id int,
     partie_id int,
     FOREIGN KEY (reponse_id) references reponse(id)
@@ -33,27 +27,4 @@ CREATE TABLE resultat (
     PRIMARY KEY (user_id, question_id),
     FOREIGN KEY (user_id) REFERENCES users(id),
     FOREIGN KEY (question_id) REFERENCES question(id)
-);
-
-CREATE TABLE score (
-    id int PRIMARY KEY auto_increment,
-    temps int NOT NULL,
-    points int NOT NULL
-);
-
-CREATE TABLE score_user (
-    user_id int,
-    score_id int,
-    points int,
-    PRIMARY KEY (user_id, score_id),
-    FOREIGN KEY (user_id) REFERENCES users(id),
-    FOREIGN KEY (score_id) REFERENCES score(id)
-);
-
-CREATE TABLE bonne_reponse (
-    question_id int,
-    reponse_id int,
-    PRIMARY KEY (question_id, reponse_id),
-    FOREIGN KEY (question_id) REFERENCES question(id),
-    FOREIGN KEY (reponse_id) REFERENCES reponse(id)
 );
