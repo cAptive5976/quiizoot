b400841fbf047ef9f7bec7ce93060a80bda0fab2
root <contact@nohanviot.fr>
2024-05-28 01:01:03 +0200

    modifié :         vues/blocs/footer.php 	modifié :         vues/blocs/head.php 	modifié :         vues/blocs/header.php

--- ctrl/ctrl_cleaner.php
+++
@@ -1,8 +0,0 @@
-<?php
-function results_cleaner_ctrl() {
-    require('crud/connection.php');
-    $c = connection();
-    require('crud/crud_functions.php');
-    results_cleaner($c);
-    header('vues/accueil.php');
-}
--- vues/connexion_admin_user.php
+++
@@ -1,9 +0,0 @@
-<?php 
-//require('blocs/header.php');
-
-echo "<a href='login_admin_user.php'><button type='button'>Se connecter</button></a>";
-echo "<h1>Bienvenue sur Quizoot ! <h1>";
-
-
-//require('blocs/footer.php');
-?>
