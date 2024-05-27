75b13b813f6ecb0f4f8ef554f3b3d2d04f2532ac
Charles <70532837+cAptive5976@users.noreply.github.com>
2024-05-27 13:08:07 +0200

    Ajout de la base de données TP3_Quizz de l'IUT #4

--- config/config.php
+++ config/config.php
@@ -1,5 +1,5 @@
 <?php
-define('HOST', '');
-define('DB', '');
-define('USER', '');
-define('PASSWORD', '');
+define('HOST', '192.168.156.221');
+define('DB', 'tp3_quizz');
+define('USER', 'cmartinj');
+define('PASSWORD', 'b92zbcKR43K8fX');
