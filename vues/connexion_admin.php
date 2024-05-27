bc726e3abbf0e2f0c4b295243f37e4af004310ac
MATTHIEU CHAILLOU <mchail09@salle1>
2024-05-27 14:22:33 +0200

    Création Vue admin et utilisateur (connexion) #11

---
+++ vues/connexion_admin.php
@@ -0,0 +1,9 @@
+<?php 
+//require('blocs/header.php');
+
+echo "<a href='login.php'><button type='button'>Se connecter</button></a>";
+echo "<h1>Bienvenue sur Quizoot ! <h1>";
+
+
+//require('blocs/footer.php');
+?>
---
+++ vues/login.php
@@ -0,0 +1,11 @@
+<?php 
+//require('blocs/header.php');
+
+
+echo "<h1>Se connecter :<h1>";
+echo "<a href='auth_admin.php'><button type='button'>Admin</a>";
+echo "<a href='auth_user.php'><button type='button'>Utilisateur</a>";
+
+
+//require('blocs/footer.php');
+?>
