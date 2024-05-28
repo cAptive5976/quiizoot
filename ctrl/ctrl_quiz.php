e16797ec9de93abfe7d996ffe73836f48c74ba6b
Nicolas <nicolas.fumoleau@etu.univ-poitiers.fr>
2024-05-28 22:29:23 +0200

    Début du développement du contrôleur de l'affichage des réponses du quiz

--- ctrl/ctrl_quiz.php
+++ ctrl/ctrl_quiz.php
@@ -1,1 +1,18 @@
 <?php
+function vue_quiz_admin() {
+    require('crud/connection.php');
+    $c = connection();
+    require('crud/crud_functions.php');
+    $id_question = $_GET['id_question'];
+    $reponses = recherche_reponses$c, $id_question);
+    
+    //View display
+    require('vues/quiz.php');
+    vue_question($id_question, $reponses);
+    
+}
+
+function temps_restant() {
+    $temps = 20;
+    
+}
