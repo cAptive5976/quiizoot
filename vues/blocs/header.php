9bca22292f79611334ab57c146e744790c9b4d8f
Nohan VIOT <contact@nohanviot.fr>
2024-05-29 11:14:07 +0200

    Modification blocs

--- vues/blocs/header.php
+++ vues/blocs/header.php
@@ -14,8 +14,8 @@
         	<nav>
         		<a href="index.php"><img src="images/Quiizoot!.png" alt="Logo de Quiizoot!" class="logo"></a>
         	     	<ul>
-        	         	<li><a href="index.php?action=apropos">A PROPOS</a></li>
-        	         	<li><a href="index.php?action=connex">SE CONNECTER</a></li>
+        	         	<li><a href="index.php?route=apropos">A PROPOS</a></li>
+        	         	<li><a href="index.php?route=login">SE CONNECTER</a></li>
         	     	</ul>
        		</nav>
      	</div>    
