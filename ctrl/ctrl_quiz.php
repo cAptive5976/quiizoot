5786e5af24a055b4e3e7f6a561dc7074caea608a
etudiant <etudiant@deb12-vdi-rt>
2024-05-29 11:48:20 +0200

    Ajout des fonctions du controleur d'affichage du quiz. Attention, code non fonctionnel

--- ctrl/ctrl_quiz.php
+++ ctrl/ctrl_quiz.php
@@ -1,18 +1,33 @@
 <?php
-function vue_quiz_admin() {
+
+$id_question = $_GET['id_question'];
+
+function ctrl_vue_quiz($id_question, $id_utilisateur) {
     require('crud/connection.php');
     $c = connection();
     require('crud/crud_functions.php');
-    $id_question = $_GET['id_question'];
-    $reponses = recherche_reponses$c, $id_question);
     
-    //View display
-    require('vues/quiz.php');
-    vue_question($id_question, $reponses);
+    $reponses = recherche_reponses($c, $id_question);
+    
+    //Affichage des réponses pour l'utilisateur
+    require('vues/vue_quiz.php');
+    vue_reponses($id_question, $id_utilisateur, $reponses);
     
 }
 
-function temps_restant() {
-    $temps = 20;
-    
+function ctrl_page_fin_quiz() {
+	require('vues/vue_quiz.php');
+	vue_page_fin_quiz();
+}
+
+function ctrl_quiz($id_question, $prenom, $nom, $classe) {
+	require('crud/connection.php');
+   $c = connection();
+   require('crud/crud_functions.php');
+	$id_utilisateur = recherche_id_utilisateur($prenom, $nom, $classe);
+	if ($id_question >= 40) {
+		ctrl_page_fin_quiz();
+	} else {
+		ctrl_vue_quiz($id_question, $id_utilisateur);
+	}
 }
