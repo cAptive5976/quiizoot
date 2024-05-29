aaadec48ea3eafa7a31b609790d1b3d033c33d51
NOHAN VIOT <nviot@salle5>
2024-05-29 10:29:03 +0200

    blocs

--- crud/crud_functions.php
+++ crud/crud_functions.php
@@ -6,18 +6,3 @@ function results_cleaner($connex) {
     $res = $connex->query($req);
     $res->closeCursor();
 }
-
-function score_classe($connex) {
-    $req = "SELECT u.prenom, u.nom, u.classe, sum(s.score) AS total_points
-        FROM resultat r
-        JOIN users u ON r.user_id = u.id
-        JOIN score s ON r.temps = s.temps
-        GROUP BY u.id
-        ORDER BY u.classe, total_points DESC
-    ";
-
-    $res = $connex->query($req);
-    $score = $res->fetchAll(PDO::FETCH_ASSOC);
-    $res->closeCursor();
-    return $score;
-}
