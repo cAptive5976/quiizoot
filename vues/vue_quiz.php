0195d5ed0f3dc55e1ffb52ecf9ab7987a5f0946d
Nicolas <nicolas.fumoleau@etu.univ-poitiers.fr>
2024-05-30 21:34:08 +0200

    suite du développement des vues

--- vues/vue_quiz.php
+++ vues/vue_quiz.php
@@ -1,20 +1,21 @@
 <?php
 function vue_reponses($id_question, $reponses, $id_utilisateur) {
-   require('vues/blocs/header.php');
+	require('vues/blocs/header.php');
     
-   echo '<h2>QUIZZ</h2>';
+	echo '<h2>QUIZZ</h2>';
     
-   echo '<h1>Question n° ' . $id_question . ' : ' . $reponses[0]['question'] . '</h1>'; //Affichage de la question à partir du tableau obtenu par la requête SQL
+	echo '<h1>Question n° ' . $id_question . ' : ' . $reponses[0]['question'] . '</h1>'; //Affichage de la question à partir du tableau obtenu par la requête SQL
+	
 	
 	echo '<form action="index.php?route=quiz?question="' . $id_question + 1 . 'method="post">'; //quand le formulaire est validé (que l'élève a choisi ses réponses et cliqué sur valider), on envoit son identifiant (champ hidden) ainsi que ses réponses au ctrl_quiz qui les enverra au crud.
 	echo '		<fieldset>
 				<legend>Choisir une ou plusieurs réponses : </legend>';
 	
 	foreach ($reponses as $rep) {
-        echo '		<p>' . $rep['reponse'] . '<input type="checkbox" value="' . $rep['id_rep'] . '"name="rep' . $rep['id_rep'] . '" /></p>';
+        echo '		<p>' . $rep['reponse'] . '<input type="checkbox" value="' . $rep['id_rep'] . '"name="' . $rep['id_rep'] . '" /></p>';
     }
 					
-	echo  '		<input type="hidden" name="id_utilisateur" value="' . $id_utilisateur . '" />';
+	echo    '	<input type="hidden" name="id_utilisateur" value="' . $id_utilisateur . '" />';
 	echo	'	<p><input type="submit" value="Valider" /></p></fieldset>
 			</form>';
     
@@ -30,9 +31,19 @@ function vue_reponses($id_question, $reponses, $id_utilisateur) {
 function vue_page_fin_quiz() {
 	require('vues/blocs/header.php');
     
-   echo '<h2>QUIZZ</h2>';
+	echo '<h2>QUIZZ</h2>';
    
-   echo '<p>Merci d\'avoir participé au quiz</p>';    
+	echo '<p>Merci d\'avoir participé au quiz</p>';    
    
-   require('vues/blocs/footer.php');
+	require('vues/blocs/footer.php');
+}
+
+function vue_attente_question_suivante() {
+	require('vues/blocs/header.php');
+    
+	echo '<h2>QUIZZ</h2>';
+   
+	echo '<p>Merci de patienter, la question suivante s\'affichera lorsque tout le monde aura répondu, ou à la fin des 20 secondes.</p>';    
+   
+	require('vues/blocs/footer.php');
 }
