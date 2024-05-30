b67e1b5f4632a47a7844de667fc68e462e5f5933
Nicolas <nicolas.fumoleau@etu.univ-poitiers.fr>
2024-05-30 21:33:45 +0200

    suite du développement des controleurs, code non fonctionnel

--- ctrl/ctrl_quiz.php
+++ ctrl/ctrl_quiz.php
@@ -2,12 +2,7 @@
 
 $id_question = $_GET['id_question']; //on récupère l'identifiant de la question depuis la route
 //controleur d'affichage de la page avec les réponses à cocher
-function ctrl_vue_quiz($id_question, $id_utilisateur) {
-    require('crud/connection.php');
-    $c = connection();
-    require('crud/crud_functions.php');
-    
-    $reponses = recherche_reponses($c, $id_question);
+function ctrl_vue_quiz($id_question, $id_utilisateur, $reponses) {
     
     //Affichage des réponses pour l'utilisateur
     require('vues/vue_quiz.php');
@@ -18,6 +13,13 @@ function ctrl_vue_quiz($id_question, $id_utilisateur) {
 function ctrl_page_fin_quiz() {
 	require('vues/vue_quiz.php');
 	vue_page_fin_quiz();
+}
+
+//controleur de l'affichage de la page d'attente de la question suivante 
+
+function ctrl_vue_page_attente_question_suivante() {
+	require('vues/vue_quiz.php');
+	vue_page_attente_question_suivante();
 }
 
 //fonction qui, à partir du prénom, du nom et de la classe, cherche l'identifiant de l'utilisateur (élève) (fonction pas encore créée dans le CRUD). Logiquement, cette fonction sera appelée une seule fois au moment de lancer le quiz, avec le nom prénom et classe de l'élève qui s'est inscrit.
@@ -31,12 +33,27 @@ function ctrl_id_user($prenom, $nom, $classe) {
 
 //fonction qui à partir de l'identifiant de la question, si celui-ci est inférieur à 40, appelle la fonction d'affichage des réponses à cocher. Si l'id de question est supérieur à 40, affiche la page de fin de quiz. Au lancement du quiz, cette fonction serait exécutée avec l'id de question 1, puis avec un id augmenté de 1 à chaque question suivante jusqu'à la 40ème.
 function ctrl_quiz($id_question) {
+	
+	require('crud/connection.php');
+    $c = connection();
+    require('crud/crud_functions.php');
+	
+	$reponses = recherche_reponses($c, $id_question);
+	
 	if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-		//ici il faut récupérer les réponses de l'élève quand il valide, je ne sais pas comment faire ça
+		//ici il faut récupérer les réponses de l'élève quand il valide, je ne sais pas vraiment comment faire ça
+		$id_utilisateur = $_POST['id_utilisateur']; //on récupère à nouveau l'id de l'utilisateur pour l'envoyer dans la base de données ensuite
+		
+		foreach ($reponses as $rep) {
+			$rep['id_rep'] = $_POST[$rep['id_rep']]; //pas sûr qu'on puisse l'écrire comme ça, mais ici on récupère les réponses de l'élève
+		}
+		
 	}
-	if ($id_question >= 40) {
+	if ($id_question >= 40) && ($question_suivante == true) { //si l'admin passe à la question suivante, et que l'id de question est supérieur à 40, on affiche la fin du quiz (je ne sais pas vraiment si il faut faire ça avec un booléen)
 		ctrl_page_fin_quiz();
-	} else {
+	} elseif ($id_question <= 40) && ($question_suivante == true) { //si l'admin passe à la question suivante, mais que l'id de question est inférieur à 40, on affiche la question suivante
 		ctrl_vue_quiz($id_question, $id_utilisateur);
+	} else { //sinon, on affiche la page d'attente de la question suivante
+		ctrl_vue_page_attente_question_suivante();
 	}
 }
