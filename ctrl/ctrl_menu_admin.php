8d7149935b3c507dfd63879f31d0ca907d71da9f
Nohan VIOT <contact@nohanviot.fr>
2024-06-03 14:58:48 +0200

    Correction

--- ctrl/ctrl_menu_admin.php
+++ ctrl/ctrl_menu_admin.php
@@ -1,7 +1,11 @@
 <?php
+session_start();
 require('vues/blocs/header.php');
 
-
+if (!isset($_SESSION['admin']) || !$_SESSION['admin']) {
+    header('Location: index.php?route=login');
+    exit;
+}
 
 require('vues/vues_admin/menu_admin.php');
 
