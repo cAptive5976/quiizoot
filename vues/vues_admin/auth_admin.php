4b0a167b6b8395b42f75a6ac4d1674e06150d43f
Nohan VIOT <contact@nohanviot.fr>
2024-06-03 15:44:53 +0200

    Add login page form

--- vues/vues_admin/auth_admin.php
+++ vues/vues_admin/auth_admin.php
@@ -1,14 +1,19 @@
 <?php
 
 include('vues/blocs/header.php');
-  
-echo '<h1> Connexion administrateur</h1>';
-echo '<p><form action="index.php?route=login_user" method="post">';
-echo '<label for="nom">Nom :</label>';
-echo '<input type="text"  name="nom" required><br><br>';
-echo '<label for="prenom">Prénom :</label>';
-echo '<input type="text" name="prenom" required><br><br>';
-echo "<button type='submit' class='button_connex'>Connexion</button>";
-echo '</form></p>';
+
+echo '<form action="index.php?route=login_user" method="post">';
+
+    echo '<h3>Connexion</h3>';
+
+    echo "<label for='username'>Nom d'utilisateur</label>";
+    echo '<input type="text" placeholder="Login" id="username">';
+
+    echo '<label for="password">Mot de passe</label>';
+    echo '<input type="password" placeholder="Password" id="password">';
+
+    echo "<button type='submit' class='button_connex'>Log In</button>";
+
+echo '</form>';
 
 include('vues/blocs/footer.php');
