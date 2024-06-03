01b741236146a28022644f982c1e92823117be7d
MATTHIEU CHAILLOU <mchail09@salle1>
2024-06-03 13:43:47 +0200

    Modif du fichier login_admin_user et login admin

--- vues/login_admin_user.php
+++ vues/login_admin_user.php
@@ -2,7 +2,7 @@
 require('blocs/header.php');
 
 echo "<h1>Se connecter :<h1>";
-echo "<a href='vues/vues_admin/auth_admin.php'><button class='button'>Admin</a>";
+echo "<a href='index.php?route=login_admin'><button class='button'>Admin</a>";
 echo "<a href='vues/vues_user/auth_user.php'><button class='button'>Utilisateur</a>";
 
 require('blocs/footer.php');
