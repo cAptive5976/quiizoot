d3684dc710961f3331fdd214e569971f7cb01326
Nohan VIOT <contact@nohanviot.fr>
2024-06-03 13:34:20 +0200

    Correction bugs

--- vues/vues_admin/menu_admin.php
+++ vues/vues_admin/menu_admin.php
@@ -1,6 +1,8 @@
-<?php //include('blocs/header.php'); ?>
+<?php 
 
-<a href='vues_admin/create_partie.php'><button type='button'>Creer Partie</button></a>
-<a href='partie_created.php'><button type='button'>Parties Existantes</button></a>
+include('../blocs/header.php');
 
-//<?php //include('blocs/footer.php'); ?>
+echo "<a href='vues_admin/create_partie.php'><button type='button'>Creer Partie</button></a>";
+echo "<a href='partie_created.php'><button type='button'>Parties Existantes</button></a>";
+
+include('../blocs/footer.php');
