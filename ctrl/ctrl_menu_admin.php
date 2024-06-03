30ece9baa897678dfc67d0555aaa7247d7250799
MATTHIEU CHAILLOU <mchail09@salle1>
2024-06-03 15:02:47 +0200

    modif fichier ctrl_menu_admin.php

--- ctrl/ctrl_menu_admin.php
+++ ctrl/ctrl_menu_admin.php
@@ -1,11 +1,7 @@
 <?php
-session_start();
+
 require('vues/blocs/header.php');
 
-if (!isset($_SESSION['admin']) || !$_SESSION['admin']) {
-    header('Location: index.php?route=login');
-    exit;
-}
 
 require('vues/vues_admin/menu_admin.php');
 
