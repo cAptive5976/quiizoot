0ed22d4da34d3380d567c47b401b5be44142eecc
MATTHIEU CHAILLOU <mchail09@salle1>
2024-06-03 14:06:45 +0200

    Modif du fichier menu_admin pour utiliser les routes

--- vues/vues_admin/menu_admin.php
+++ vues/vues_admin/menu_admin.php
@@ -1,8 +1,4 @@
 <?php 
 
-include('../blocs/header.php');
-
 echo "<a href='vues_admin/create_partie.php'><button type='button'>Creer Partie</button></a>";
 echo "<a href='partie_created.php'><button type='button'>Parties Existantes</button></a>";
-
-include('../blocs/footer.php');
