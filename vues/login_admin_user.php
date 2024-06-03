eddede9623496c0dbba68d101fd8ff70c4967dda
Nohan VIOT <contact@nohanviot.fr>
2024-06-03 14:15:20 +0200

    CSS

--- vues/login_admin_user.php
+++ vues/login_admin_user.php
@@ -2,7 +2,7 @@
 require('blocs/header.php');
 
 echo "<h1>Se connecter :<h1>";
-echo "<a href='index.php?route=login_admin'><button class='button'>Admin</a>";
-echo "<a href='vues/vues_user/auth_user.php'><button class='button'>Utilisateur</a>";
+echo "<a href='index.php?route=login_admin' class='button_login_admin_user'>Admin</a>";
+echo "<a href='index.php?route=login_user' class='button_login_admin_user'>Utilisateur</a>";
 
 require('blocs/footer.php');
