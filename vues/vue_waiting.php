06fc65c2d3d326e4a876bc84ac940acd6d5ed8e0
Charles <70532837+cAptive5976@users.noreply.github.com>
2024-06-06 15:19:11 +0200

    Ajout du CSS pour la file d'attente #13

--- vues/vue_waiting.php
+++ vues/vue_waiting.php
@@ -1,11 +1,13 @@
 <?php
 function show_waiting($queue) {
     require('blocs/header.php'); ?>
+    <h1>File d'attente du Quiz</h1>
     <div id="queue">
         <?php
         $queueHtml = '';
+        // Ici on a une boucle qui affiches les utilisateurs petit a petit
         foreach ($queue as $user) {
-            $queueHtml .= "<p><span class='prenom'>" . htmlspecialchars($user['prenom']) . "</span> " .
+            $queueHtml .= "<p class='joueur'><span class='prenom'>" . htmlspecialchars($user['prenom']) . "</span> " .
                           "<span class='nom'>" . htmlspecialchars($user['nom']) . "</span> " .
                           "<span class='classe'>" . htmlspecialchars($user['classe']) . "</span></p>\n";
         }
