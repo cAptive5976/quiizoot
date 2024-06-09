2141c24fe4339cde3acc906b1a9f39155bb8af82
etudiant <etudiant@deb12-vdi-rt>
2024-06-09 16:08:56 +0200

    modifications sur le code des controleurs du quiz et ajout en commentaire d'une fonction crud permttant d'insérer le temps de réponse de l'élève

--- ctrl/ctrl_quiz.php
+++ ctrl/ctrl_quiz.php
@@ -1,6 +1,7 @@
 <?php
 
-$id_question = $_GET['id_question']; //on récupère l'identifiant de la question depuis la route
+
+
 //controleur d'affichage de la page avec les réponses à cocher
 function ctrl_vue_quiz($id_question, $id_utilisateur, $reponses) {
     
@@ -22,8 +23,8 @@ function ctrl_vue_page_attente_question_suivante() {
 	vue_page_attente_question_suivante();
 }
 
-//fonction qui, à partir du prénom, du nom et de la classe, cherche l'identifiant de l'utilisateur (élève) (fonction pas encore créée dans le CRUD). Logiquement, cette fonction sera appelée une seule fois au moment de lancer le quiz, avec le nom prénom et classe de l'élève qui s'est inscrit.
-
+//fonction qui, à partir du prénom, du nom et de la classe, cherche l'identifiant de l'utilisateur (élève). Logiquement, cette fonction sera appelée une seule fois au moment de lancer le quiz, avec le nom prénom et classe de l'élève qui s'est inscrit.
+//cette fonction devrait peut être plutôt être appelée dans la file d'attente
 function ctrl_id_user($prenom, $nom, $classe) {
 	require('crud/connection.php');
 	$c = connection();
@@ -32,63 +33,20 @@ function ctrl_id_user($prenom, $nom, $classe) {
 }
 
 <<<<<<< HEAD
-//fonction pour trouver l'id utilisateur à partir du nom, du prénom et de la classe de l'élève :
 
-//function recherche_id_utilisateur($connex, $prenom, $nom, $classe) {
 //
-//		$req = "SELECT u.id FROM users u WHERE u.prenom = :prenom AND u.nom = :nom AND u.classe = :classe";
+//	Ici faire une requête SQL pour insérer le temps de réponse de l'élève
+//
+// Proposition de fonction CRUD (à vérifier par Charles qui fait les fonctions CRUD)
+
+// function insertionTempsReponse($id_utilisateur, $id_question, $duree_reponse) {
+//		$req = "INSERT INTO resultat (user_id, question_id, temps) VALUES (:id_utilisateur, :id_question, :duree_reponse)";
 //    $res = $connex->prepare($req);
-//    $res->bindParam(':prenom', $prenom, PDO::PARAM_STR);
-//    $res->bindParam(':nom', $nom, PDO::PARAM_STR);
-//		$res->bindParam(':classe', $classe, PDO::PARAM_STR);
+//    $res->bindParam(':id_utilisateur', $id_utilisateur, PDO::PARAM_STR);
+//    $res->bindParam(':id_question', $id_question, PDO::PARAM_STR);
+//    $res->bindParam(':duree_reponse', $duree_reponse, PDO::PARAM_STR);
 //    $res->execute();
-//    $id_utilisateur = $res->fetchAll(PDO::FETCH_ASSOC);
-//    $res->closeCursor();
-//		return $id_utilisateur;
 //}
-
-//fonction crud pour afficher toutes les réponses à cocher selon l'id de question
-
-//function recherche_reponses($connex, $id_question) {
-//		$req = "SELECT id, enonce_reponse FROM reponse WHERE question_id = :id;
-//		$res = $connex->prepare($req);
-//		$res->bindParam(':id', $id_question, PDO::PARAM_STR);
-//    $res->execute();
-//    $reponses = $res->fetchAll(PDO::FETCH_ASSOC);
-//    $res->closeCursor();
-//		return $reponses;
-//}
-
-//fonction crud pour afficher l'énoncé de la question a partir de son id
-
-//function recherche_question($connex, $id_question) {
-//		$req = "SELECT id, enonce FROM question WHERE id = :id;
-//		$res = $connex->prepare($req);
-//		$res->bindParam(':id', $id_question, PDO::PARAM_STR);
-//    $res->execute();
-//    $question = $res->fetchAll(PDO::FETCH_ASSOC);
-//    $res->closeCursor();
-//		return $question;
-//}
-
-//fonction crud pour afficher les réponses correctes (/!\ ATTENTION LA REQUETE SQL POURRAIT ETRE FAUSSE /!\)
-//
-//function recherche_bonnes_reponses($connex, $id_question) {
-//
-//		$req = "SELECT question.enonce AS question, reponse.enonce_reponse AS reponse, reponse.id AS id_rep FROM question INNER JOIN reponse ON question.reponse_id = reponse.id WHERE question.id = :id";
-//		$res = $connex->prepare($req);
-//		$res->bindParam(':id', $id_question, PDO::PARAM_STR);
-//		$res->execute();
-//		$reponses = $res->fetchAll(PDO::FETCH_ASSOC);
-//		$res->closeCursor();
-//		return $reponses;
-//
-//}
-
-//fonction crud pour insérer le score de l'élève
-//
-//	Ici faire une requête SQL pour insérer le score de l'élève en fonction de son temps de réponse
-//
 
 function calculerDureeEnSecondes($startTime, $endTime) {
     // Convertir les temps en timestamps
@@ -101,113 +59,38 @@ function calculerDureeEnSecondes($startTime, $endTime) {
     return $dureeEnSecondes;
 }
 
-function calculScore($dureeEnSecondes) {
-	//calcul du score selon le temps mis à répondre
-	switch ($dureeEnSecondes) {
- 		case 0 <= $dureeEnSecondes < 1:
- 			return 1000;
- 		break;
- 		case 1 <= $dureeEnSecondes < 2:
- 			return 950;
- 		break;
- 		case 2 <= $dureeEnSecondes < 3:
- 			return 900;
- 		break;
- 		case 3 <= $dureeEnSecondes < 4:
- 			return 850;
- 		break;
- 		case 4 <= $dureeEnSecondes < 5:
- 			return 800;
- 		break;
- 		case 5 <= $dureeEnSecondes < 6:
- 			return 750;
- 		break;
- 		case 6 <= $dureeEnSecondes < 7:
- 			return 700;
- 		break;
- 		case 7 <= $dureeEnSecondes < 8:
- 			return 650;
- 		break;
- 		case 8 <= $dureeEnSecondes < 9:
- 			return 600;
- 		break;
- 		case 9 <= $dureeEnSecondes < 10:
- 			return 550;
- 		break;
- 		case 10 <= $dureeEnSecondes < 11:
- 			return 500;
- 		break;
- 		case 11 <= $dureeEnSecondes < 12:
- 			return 450;
- 		break;
- 		case 12 <= $dureeEnSecondes < 13:
- 			return 400;
- 		break;
- 		case 13 <= $dureeEnSecondes < 14:
- 			return 350;
- 		break;
- 		case 14 <= $dureeEnSecondes < 15:
- 			return 300;
- 		break;
- 		case 15 <= $dureeEnSecondes < 16:
- 			return 250;
- 		break;
- 		case 16 <= $dureeEnSecondes < 17:
- 			return 200;
- 		break;
- 		case 17 <= $dureeEnSecondes < 18:
- 			return 150;
- 		break;
- 		case 18 <= $dureeEnSecondes < 19:
- 			return 100;
- 		break;
- 		case 19 <= $dureeEnSecondes < 20:
- 			return 50;
- 		break;
- 		case $dureeEnSecondes >= 20:
- 			return 0;
- 		break;
-	 	default:
- 			return 0;
- 		break;
-}
-}
 
 
-
-function recup_reponses_eleve() {
+function recup_reponses_eleve($id_utilisateur, $id_question, $heureDebut) {
 	require('crud/connection.php');
    $c = connection();
    require('crud/crud_functions.php');
 	if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 		//ici il faut récupérer les réponses de l'élève quand il valide, je ne sais pas vraiment comment faire ça
-		$id_utilisateur = $_POST['user_id']; //on récupère à nouveau l'id de l'utilisateur pour l'envoyer dans la base de données ensuite
-		$question_suivante = $_SESSION['question_suivante']; //ici on récupère un paramètre de session 'question suivante' qui sera true si l'administrateur envoie la page de la question suivante, pas sûr qu'il faut faire comme ça pour gérer le passage à la question suivante par l'admin
 		
 		$dateTimeFin = new DateTime();
 		$heureFin = $dateTimeFin->date("Y-m-d H:i:s");
-		$duree_reponse = calculerDureeEnSecondes($heureDebut, $heureFin);
-			
+		$duree_reponse = floor(calculerDureeEnSecondes($heureDebut, $heureFin)); //on calcule la durée en secondes et on arrondit à la seconde inférieure
+		
 		foreach ($bonnes_reponses as $rep) {
-			if (!isset($_POST[$rep['id_rep']]) {
-				//ici il faudrait calculer le score de l'élève en fonction de ses réponses justes et du temps qu'il a mis à répondre
-				$score = 0;
+			if (!isset($_POST[strval($rep['id_rep'])]) {
+				break; //si on trouve une mauvaise réponse, on casse la boucle et le temps de réponse n'est pas enregistré
 			} else {
-				$score = calculScore($duree_reponse);
+				insertionTempsReponse($id_utilisateur, $id_question, $duree_reponse);
 			}
 		}
-			
-		insertionScore($id_utilisateur, $score);
-		
 	}
 }
 
 =======
 >>>>>>> 4783acd8c486481c2c856c8f62591f7e881c0c20
 //fonction qui à partir de l'identifiant de la question, si celui-ci est inférieur à 40, appelle la fonction d'affichage des réponses à cocher. Si l'id de question est supérieur à 40, affiche la page de fin de quiz. Au lancement du quiz, cette fonction serait exécutée avec l'id de question 1, puis avec un id augmenté de 1 à chaque question suivante jusqu'à la 40ème.
-function ctrl_quiz($id_question) {
+function ctrl_quiz() {
 	
-	
+	$id_question = $_GET['id_question']; //on récupère l'identifiant de la question depuis la route, ainsi que l'id utilisateur
+	$id_utilisateur = $_GET['id_user'];
+	$question_suivante = $_SESSION['question_suivante']; //ici on récupère un paramètre de session 'question suivante' qui sera true si l'administrateur envoie la page de la question suivante, pas sûr qu'il faut faire comme ça pour gérer le passage à la question suivante par l'admin
+		
 	$question = recherche_question($c, $id_question);
 	$reponses = recherche_reponses($c, $id_question);
 	$bonnes_reponses = recherche_bonnes_reponses($c, $id_question);
@@ -219,6 +102,9 @@ function ctrl_quiz($id_question) {
 			$dateTimeDebut = new DateTime();
 			$heureDebut = $dateTimeDebut->date("Y-m-d H:i:s");
 			ctrl_vue_quiz($id_question, $id_utilisateur, $reponses);
+			if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+				recup_reponses_eleve($id_utilisateur, $id_question-1, $heureDebut); //comme ce controleur est appelé avec l'id de question suivante lorsque l'élève envoie ses réponse, afin de pouvoir enregistrer le temps de la réponse à la question à laquelle il vient de répondre, on enlève 1 à l'id de question
+			}
 			
 	} elseif ($question_suivante == false) { //sinon, on affiche la page d'attente de la question suivante
 		ctrl_vue_page_attente_question_suivante();
