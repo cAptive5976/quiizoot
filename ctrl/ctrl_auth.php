c194dd7986aea084be1eb7a525c8b4b4725e7764
etudiant <etudiant@deb12-vdi-rt>
2024-06-12 10:54:53 +0200

    modif : ajout de commentaires #37

--- ctrl/ctrl_auth.php
+++ ctrl/ctrl_auth.php
@@ -48,30 +48,43 @@ function login_user() {
 // de login, vérifie si l'administrateur existe et si le mot de passe est correct. Si c'est le
 // cas, elle crée une session pour l'administrateur. Sinon, elle renvoie une erreur de login.
 
+
 function login_admin() {
+    // Vérifie si la méthode de la requête est POST
     if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        // Récupère les données du formulaire
         $prenom = $_POST['prenom'];
         $nom = $_POST['nom'];
         $password = $_POST['password'];
 
+        // Inclut le fichier de connexion à la base de données
         require('crud/connection.php');
+        // Établit la connexion à la base de données
         $c = connection();
+        // Inclut les fonctions CRUD nécessaires
         require('crud/crud_functions.php');
+        // Tente de récupérer les informations de l'admin via les fonctions CRUD
         $admin = login_admin_crud($c, $prenom, $nom);
 
+        // Vérifie si l'admin existe et si le mot de passe est correct
         if ($admin && $password === $admin['password']) {
+            // Initialise les variables de session pour l'admin connecté
             $_SESSION['admin'] = true;
             $_SESSION['user'] = $admin['prenom'] . ' ' . $admin['nom'];
             $_SESSION['role'] = 'admin';
             $_SESSION['green_alert'] = "Bienvenue " . $admin['prenom'];
+            // Redirige l'admin vers la page du menu admin
             header('Location: index.php?route=menu_admin');
             exit;
         } else {
+            // En cas d'échec de connexion, met à jour l'alerte de session
             $_SESSION['red_alert'] = "Login ou mot de passe incorrect";
+            // Redirige vers la page de login admin
             header('Location: index.php?route=login_admin');
             exit;
         }
     } else {
+        // Si la méthode de la requête n'est pas POST, affiche le formulaire de login admin
         require('vues/vues_admin/auth_admin.php');
     }
 }
